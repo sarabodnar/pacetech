a7ab69bf99d771c89dd3ec9c7f730669b8582106
SARA BODNAR <100146285+sarabodnar@users.noreply.github.com>
2022-03-18 22:37:34 -0300

    Missão semana 06
    conversor de temperatura e moeda

---
+++ missao6.java
@@ -0,0 +1,74 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package projetoConversor;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Sara Bodnar
+ */
+public class missao6 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       Scanner leitor = new Scanner(System.in);
+        System.out.println("####### MENU #######");
+        System.out.println("1-Conversor de Temperatura");
+        System.out.println("2-Conversor de Moeda");
+        System.out.println("3-Não desejo converter agora");
+
+        
+        int opcao = leitor.nextInt();
+        System.out.println("Digite uma opção para iniciar o menu:");
+
+        if (opcao == 1) {
+            System.out.println("1- Fahrenheit para Celsius");
+            System.out.println("2- Celsius para Fahrenheit");
+            System.out.println("Digite uma opção:");
+            int escolhaTemperatura = leitor.nextInt();
+            if (escolhaTemperatura == 1) {
+                double temperatura = leitor.nextDouble();
+                System.out.println("Informe a temperatura em Farhrenheit:");
+                double temperaturaConver = (5 / 9) * (temperatura - 32);
+                System.out.println("Temperatura convertida para Celsius:" + temperaturaConver + "ºC");
+            }
+            if (escolhaTemperatura == 2) {
+                System.out.println("Informe a temperatura em Celcius:");
+                double temperatura = leitor.nextDouble();
+                double temperaturaConver;
+                temperaturaConver = (temperatura * (9 / 5)) + 32;
+                System.out.println("Temperatura convertida para Fahrenheit:" + temperaturaConver + "ºF");
+            } else if (opcao == 2) {
+                System.out.println("Qual a cotação do dólar para hoje?");
+                double cotacaoDolar = leitor.nextDouble();
+                System.out.println("1 - Converter dólar para real");
+                System.out.println("2 - Conversor real para dólar");
+                int escolhaMoeda = leitor.nextInt();
+                System.out.println("Digite uma opção: ");
+                if (escolhaMoeda == 1) {
+                    System.out.println("Informe o valor em dólares: ");
+                    double moeda = leitor.nextDouble();
+                    double moedaConver = moeda * cotacaoDolar;
+                    System.out.println("Valor em real: R$ " + moedaConver);
+                }
+                if (escolhaMoeda == 2) {
+                    System.out.println("Informe o valor em reais: ");
+                    double moeda = leitor.nextDouble();
+                    double moedaConver = moeda / cotacaoDolar;
+                    System.out.println("Valor em dólar: U$ " + moedaConver);
+                }
+
+            }
+        }
+        if (opcao ==3){
+            System.out.println("Terminamos por aqui!");
+        }
+        leitor.close();
+    }
+    
+}
